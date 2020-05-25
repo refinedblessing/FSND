cce69c48255a810e4f43d008f886f876819e96df
Blessing Ebowe <blessing@whisper.sh>
2020-05-25 02:21:13 -0700

    Update Api

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
-/* @TODO replace with your variables
+/*
  * ensure all variables on this page match your project
  */
 
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'refinedblessing-coffee-shop', // the auth0 domain prefix
+    audience: 'coffee-dev', // the audience set for the auth0 app
+    clientId: 'V0TjMtFe9w51gzgothO9I171YLrVcTlG', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
